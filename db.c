e382447f48831cb44c5b750cfb59467ad005522e
David Lin <lindavid1998@gmail.com>
2025-02-12 18:33:12 -0800

    Initial commit

---
+++ db.c
@@ -0,0 +1,16 @@
+int main(int argc, char **argv) {
+    // while true
+    // prompt user for command
+    // if command == ".exit"
+    // break out of the loop
+    
+    char input[200];
+
+    while (true) {
+
+        if strcmp(argv[1], ".exit") {
+            break;
+        }
+    }
+    return 0
+}
