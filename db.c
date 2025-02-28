6319799b3c46730beb73f01d6acd374c5d582d38
David Lin <lindavid1998@gmail.com>
2025-02-28 14:41:14 -0800

    Add switch statement to handle meta commands

--- db.c
+++ db.c
@@ -10,6 +10,12 @@ typedef struct
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
 /*
 Init InputBuffer object
 */
@@ -47,23 +53,47 @@ void close_input_buffer(InputBuffer *input_buffer)
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     InputBuffer *input_buffer = new_input_buffer();
-    
+
     while (true)
     {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        // if (strcmp(input_buffer->buffer, ".exit") == 0)
+        // {
+        //     close_input_buffer(input_buffer);
+        //     exit(EXIT_SUCCESS);
+        // }
+
+        if (input_buffer->buffer[0] == '.')
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+            switch (do_meta_command(input_buffer))
+            {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                continue;
+            }
         }
-        else
-        {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-        }
+
+        // prepare result by converting input to Statement
+        // execute statement
     }
 }
